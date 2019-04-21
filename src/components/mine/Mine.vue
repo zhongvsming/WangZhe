1a1fb3923222a8336a793269294ac64e223dd2f5
zhong <234247845@qq.com>
2019-04-21 10:14:10 +0800

    mine base complited

--- src/components/mine/Mine.vue
+++ src/components/mine/Mine.vue
@@ -7,7 +7,18 @@
             <div class="login-btn">
                 <button>点击登录</button>
             </div>    
-        </div>    
+        </div> 
+        <div class="mine-btns">
+            <div class="mine-btns-row1">
+                 <a name="setting"><Icon type="md-settings" size="24"/>账户设置<Icon type="md-arrow-dropright" size="24"/></a>
+                 <a><Icon type="md-create" size="24"/>意见反馈<Icon type="md-arrow-dropright" size="24"/></a>
+            </div>
+            <div class="mine-btns-row2">
+                 <a name="contact"><Icon type="md-information-circle" size="24"/>用户协议<Icon type="md-arrow-dropright" size="24"/></a>
+                 <a><Icon type="md-pizza" size="24"/>版本号<Icon type="md-arrow-dropright" size="24"/></a>
+            </div>
+            
+        </div>       
     </div>
 </template>
 
@@ -43,11 +54,46 @@
 .login-btn{
     button{
         margin-top:15px;
-        // width:70px;
-        // height:35px;
-        font-size:1.3em;
-        padding:7px;
+        font-size:1em;
+        padding:10px;
         background:white;
     }
 }
+.mine-btns-row1{
+    margin-top:35px;
+    a{
+        // width:100%;
+        height:50px;
+        background:white;
+        display:block;
+        display:flex;
+        justify-content:space-between;
+        align-items:center;
+        padding:0 10px;
+        // border-top:1px #c3c3c3 solid;
+        border-bottom:1px #c3c3c3 solid;
+    }
+    [name=setting]{
+        border-top:1px #c3c3c3 solid;
+    }
+}    
+.mine-btns-row2{
+    margin-top:35px;
+    a{
+        // width:100%;
+        height:50px;
+        background:white;
+        display:block;
+        display:flex;
+        justify-content:space-between;
+        align-items:center;
+        padding:0 10px;
+        // border-top:1px #c3c3c3 solid;
+        border-bottom:1px #c3c3c3 solid;
+    }
+    [name=contact]{
+        border-top:1px #c3c3c3 solid;
+    }
+}    
+
 </style>
